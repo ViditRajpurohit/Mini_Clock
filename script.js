308c6460fb4ed128956121ad4a2a09d988e98211
ViditRajpurohit <viditrajpurohit27@gmail.com>
2024-02-21 11:40:46 +0530

    hello

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let clock = document.querySelector("#clock");
 setInterval(() => {
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString()
+    console.log("hello");
     
 
 },1000)
